29d710041676f0675986cd5c0d7d84ec7447dad4
Travis Clarkson <tk1667@gmail.com>
2018-02-26 01:56:00 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -25,7 +25,13 @@ class Transfer
   end
 
   def reverse_transfer
-
+    if @status == "complete"
+      @sender.balance += @amount
+      @receiver.balance -= @amount
+      @status = "reversed"
+    else
+      #
+    end
   end
 
 end
