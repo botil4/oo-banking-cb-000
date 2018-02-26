ddc8d7c2b9b4046229fc3b20c7881a34767195fc
Travis Clarkson <tk1667@gmail.com>
2018-02-26 01:25:51 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,13 @@
 class BankAccount
 
+  attr_accessor :status, :balance, :name
+  attr_writer
+  attr_reader
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
 end
